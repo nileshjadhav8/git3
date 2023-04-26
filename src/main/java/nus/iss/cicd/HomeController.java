918076d1e647bf4b3b24bd5b96b5f432ace448f8
Nilesh <nil.y.jadhav@gmail.com>
2023-04-26 14:48:26 +0800

    main branch change

--- src/main/java/nus/iss/cicd/HomeController.java
+++ src/main/java/nus/iss/cicd/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        System.out.println("changed");
+        System.out.println("main branch changed");
 
         System.out.println("This is controller class");
         return "index";
