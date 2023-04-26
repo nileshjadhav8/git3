ba2d9d6b3c29013307e2fd70cb4198df503a2921
Nilesh <nil.y.jadhav@gmail.com>
2023-04-26 15:16:03 +0800

    for fix

--- src/main/java/nus/iss/cicd/HomeController.java
+++ src/main/java/nus/iss/cicd/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        System.out.println("develop branch changed again");
+        System.out.println("develop branch changed again1");
 
         System.out.println("This is controller class");
         return "index";
